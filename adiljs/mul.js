d1b54f2da91153d8d90c5a1e77e5bf2b08cb5a11
adhil-bit <67473659+adhil-bit@users.noreply.github.com>
2022-09-04 00:37:29 +0530

    Update mul.js
    added comments

--- adiljs/mul.js
+++ adiljs/mul.js
@@ -1,3 +1,3 @@
-let x = 5;
-let y = 2;
-console.log( z = x * y);
+let x = 5; //defining x as 5
+let y = 2; //defining y as 2
+console.log( z = x * y); // constructing another z as multiplier with respect to x&y
