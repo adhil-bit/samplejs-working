fc7e034c8e681efc30214693bb505c0fb5848ec8
adhil-bit <67473659+adhil-bit@users.noreply.github.com>
2022-09-04 00:32:15 +0530

    Update boolean.js
    added comments

--- adiljs/boolean.js
+++ adiljs/boolean.js
@@ -1,5 +1,5 @@
-let x = 5;
-let y = 5;
-let z = 6;
-console.log(x == y) ;      // Returns true
-(x == z)       // Returns false1
+let x = 5;  //memory allocation for x is 5
+let y = 5;  //memory allocation for y is 5
+let z = 6;  //memory allocation for z is 6
+console.log(x == y) ;      // printing if Returns true
+console.log(x == z) ;      // or Returns false1
