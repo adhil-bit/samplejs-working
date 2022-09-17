edd10c55b311f0b26a2939b01a1b06454511ac26
adhil@123 <67473659+adhil-bit@users.noreply.github.com>
2022-09-17 09:56:21 +0530

    popfn js

---
+++ 2pop.js
@@ -0,0 +1,8 @@
+const plants = ['thulasi', 'onion', 'cucumbr', 'carrot', 'chilly'];
+
+console.log(plants.pop()); //removing last string from array
+console.log(plants);
+console.log(plants.pop()); //removing last string from array
+console.log(plants);
+console.log(plants.pop()); //removing last string from array
+console.log(plants);
