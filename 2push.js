a34272d2bd30ee389a67d64d412e6b028765b7ec
adhil@123 <67473659+adhil-bit@users.noreply.github.com>
2022-09-17 09:52:15 +0530

    js

---
+++ 2push.js
@@ -0,0 +1,5 @@
+const names = ['arun', 'varun', 'hilp']; //array defining
+
+names.push('ashraf'); //external string wishing to add with existimg array
+
+console.log(names); // array will add ashraf string with existing array as o/p
